42b20a055242e21698635cdbba257ce36bca6f1f
Ken Wilcox <wilcoxk@gmail.com>
2015-08-08 19:43:20 -0600

    somethings broken

--- app.js
+++ app.js
@@ -5,8 +5,13 @@
   app.controller('WordController', ['$http', function ($http) {
     var words = this;
     words.prek = [ ];
+    words.word = "Are you ready to play?";
     $http.get('/prek.json').success(function (data) {
       words.prek = data;
     });
+
+    function getCard() {
+      words.word = words.prek[Math.random() * (words.prek.length)];
+    }
   }]);
  }());
