6b16aca38dd62fb9692dcfc8843f100172d3e6bc
Ken Wilcox <wilcoxk@gmail.com>
2015-08-10 20:22:24 -0600

    first version now working

--- app.js
+++ app.js
@@ -3,18 +3,43 @@
   'use strict';
   var app = angular.module('words', []);
   app.controller('WordController', ['$http', function ($http) {
-    var words = this;
-    words.prek = [ ];
-    words.word = "Are you ready to play?";
-    $http.get('/prek.json').success(function (data) {
-      words.prek = data;
-    });
+    var vm = this;
+    vm.prek = [ ];
+    vm.prekCache = [ ];
+    vm.word = "Are you ready to play?";
+    vm.count = 0;
+    vm.wordNo = 0;
+    vm.caption = "Yes";
 
-    this.getCard = function () {
-      var index = Math.floor(Math.random() * words.prek.length);
-      var card = words.prek[index];
-      //console.log(card);
-      words.word = card.word;
+    this.loadData = function() {
+      $http.get('/prek.json').success(function (data) {
+        vm.prek = data;
+        vm.prekCache = data;
+        vm.count = data.length;
+      });     
     };
+ 
+    this.getCard = function () {
+      if (vm.caption === "Start Over") {
+        this.loadData();
+        vm.word = "Are you ready to play?";
+        vm.caption = "Yes";
+        vm.wordNo = 0;
+      }
+
+      var index = Math.floor(Math.random() * vm.prekCache.length);
+      var card = vm.prekCache[index];
+      vm.prekCache.splice(index, 1);
+      vm.word = card.word;
+      vm.wordNo = vm.count - vm.prekCache.length;
+      
+      if (vm.wordNo === vm.count) {
+        vm.caption = "Start Over";
+      } else {
+        vm.caption = "Next";
+      }
+    };
+
+    this.loadData();
   }]);
  }());
